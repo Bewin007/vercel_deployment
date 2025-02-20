5518888a2f1c789dd4a926c6996b00af82037c3b
Bewin007 <bewinfelix@karunya.edu.in>
2025-02-20 08:21:24 +0530

    updated conf

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,17 @@
-# build_files.sh
-pip install -r requirements.txt
-python3.9 manage.py collectstatic
+#!/bin/bash
+
+# Exit immediately if any command fails
+
+# Install all dependencies
+pip install asgiref==3.8.1 \
+            Django==5.0.6 \
+            django-cors-headers==4.3.1 \
+            djangorestframework==3.15.1 \
+            djangorestframework-simplejwt==5.3.1 \
+            PyJWT==2.8.0 \
+            sqlparse==0.5.0 \
+            typing_extensions==4.12.2 \
+            tzdata==2024.1
+
+# Collect static files
+python3.9 manage.py collectstatic 
